0ad8cab840917bf901fa1c6186256325fe5a8c60
JoannaBragiel <66517471+JoannaBragiel@users.noreply.github.com>
2021-09-09 15:07:35 +0200

    Update models.py

--- records/models.py
+++ records/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class BookUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
 
 class Publication(models.Model):
     title = models.CharField(max_length=150)
