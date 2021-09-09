beaca01d65c05564af93b293e883cc3c4ad64469
JoannaBragiel <66517471+JoannaBragiel@users.noreply.github.com>
2021-09-09 15:08:48 +0200

    Update settings.py

--- library/settings.py
+++ library/settings.py
@@ -22,12 +22,13 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6tqqe)7#g01ofr76o*de_%xo0k&lh_ihn@4x5=o%ibp^j9u9u!'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://bookfinder.pythonanywhere.com/']
 
 
 # Application definition
